0492c53a8ba95a25ed8bbeab138660bf29769d49
Bram Ceulemans <bram@ceulemans.dev>
2022-02-24 10:30:03 +0100

    Add comments

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -18,28 +18,12 @@ class InstallCommand extends Command
 
     public function handle(Application $application): int
     {
-        $domain = $this->getDomain();
+        $path = $this->getDockerComposePath($application);
+        $yaml = $this->parseDockerCompose($path);
 
-        $path = $application->basePath('docker-compose.yml');
-        $yaml = Yaml::parseFile($path);
+        Arr::set($yaml, 'networks.yacht', $this->getNetwork());
 
-        Arr::set($yaml, 'networks.yacht', [
-            'name' => 'yacht-proxy',
-            'external' => true,
-        ]);
-
-        $services = Arr::get($yaml, 'services', []);
-
-        foreach ($services as $name => &$service) {
-            if ($name === 'laravel.test') {
-                Arr::set($service, 'ports', [':80']);
-                Arr::set($service, 'networks', $this->getNetworks($service));
-                Arr::set($service, 'labels', $this->getLabels($domain));
-            } else {
-                Arr::forget($service, 'ports');
-            }
-        }
-
+        $services = $this->configureServices(Arr::get($yaml, 'services', []));
         Arr::set($yaml, 'services', $services);
 
         $this->save($yaml, $path);
@@ -47,6 +31,28 @@ class InstallCommand extends Command
         return self::SUCCESS;
     }
 
+    public function configureServices(array $services): array
+    {
+        foreach ($services as $name => &$service) {
+            if ($name === 'laravel.test') {
+                $domain = $this->getDomain();
+
+                Arr::set($service, 'labels', $this->getLabels($domain));
+                Arr::set($service, 'networks', $this->getNetworks($service));
+                Arr::set($service, 'ports', [':80']);
+            } else {
+                Arr::forget($service, 'ports');
+            }
+        }
+
+        return $services;
+    }
+
+    /**
+     * Prompt the user for a domain to use.
+     *
+     * @return string
+     */
     public function getDomain(): string
     {
         $domain = $this->argument('domain');
@@ -59,6 +65,12 @@ class InstallCommand extends Command
         return $domain;
     }
 
+    /**
+     * Get the list of networks that a service should be in, when exposed to Traefik.
+     *
+     * @param array $service
+     * @return array
+     */
     public function getNetworks(array $service): array
     {
         return Collection::make(Arr::get($service, 'networks', []))
@@ -69,6 +81,12 @@ class InstallCommand extends Command
             ->toArray();
     }
 
+    /**
+     * Get the labels associated with the "laravel.test" container.
+     *
+     * @param string $domain the domain name
+     * @return string[] labels
+     */
     public function getLabels(string $domain): array
     {
         $slug = Str::slug($domain);
@@ -82,6 +100,35 @@ class InstallCommand extends Command
     {
         $content = Yaml::dump($yaml, 4);
         file_put_contents($path, $content);
+    }
+
+    /**
+     * @return array
+     */
+    public function getNetwork(): array
+    {
+        return [
+            'name' => 'yacht-proxy',
+            'external' => true,
+        ];
+    }
+
+    /**
+     * @param string $path
+     * @return mixed
+     */
+    public function parseDockerCompose(string $path): mixed
+    {
+        return Yaml::parseFile($path);
+    }
+
+    /**
+     * @param Application $application
+     * @return string
+     */
+    public function getDockerComposePath(Application $application): string
+    {
+        return $application->basePath('docker-compose.yml');
     }
 
     protected function getArguments(): array
